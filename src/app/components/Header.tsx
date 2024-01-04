e757b36b50050203a499548f1d5bad23940dfab2
Alfredo <alfredoeb96@gmail.com>
2024-01-04 15:29:05 -0800

    [prisma] fixed prisma types to take in role and made adapter take in current postgres db

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,48 +1,68 @@
 "use client";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu} from "@nextui-org/react";
-import { useSession } from 'next-auth/react';
-
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+} from "@nextui-org/react";
+import { useSession } from "next-auth/react";
 
 export default function Header() {
-    const { data: data, status } = useSession();
+  const { data: data, status } = useSession();
 
-    return (
-        <header>
-            <Navbar>
-                <NavbarBrand>
-                    <Link href="/" className="font-bold text-inherit">MLG</Link>
-                </NavbarBrand>
-                <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                    {status === "authenticated" && data !== undefined ? (
-                        <Dropdown>
-                            <NavbarItem>
-                                <DropdownTrigger>
-                                    <Button>Create</Button>
-                                </DropdownTrigger>
-                            </NavbarItem>
-                            <DropdownMenu>
-                                <DropdownItem key="tournaments" className="test hover:border-pink-500 hover:border-2" >
-                                    <Link  href="/create/tournaments">Tournaments</Link>
-                                </DropdownItem>
-                                <DropdownItem className=" border-pink-700"  key="game_category">
-                                    <Link className=" border-pink-700"  href="/create/game-category">Game Category</Link>
-                                </DropdownItem>
-                            </DropdownMenu>
-                        </Dropdown>
-                    ) : (
-                        <>
-                            <NavbarItem className="hidden lg:flex">
-                                <Link href="/auth/sign-in">Login</Link>
-                            </NavbarItem>
-                            <NavbarItem>
-                                <Button as={Link} color="primary" href="#" variant="flat">
-                                    Sign Up
-                                </Button>
-                            </NavbarItem>
-                        </>
-                    )}
-                </NavbarContent>
-            </Navbar>
-        </header>
-    )
+  return (
+    <header>
+      <Navbar>
+        <NavbarBrand>
+          <Link href="/" className="font-bold text-inherit">
+            MLG
+          </Link>
+        </NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          {status === "authenticated" && data !== undefined ? (
+            <Dropdown>
+              <NavbarItem>
+                <DropdownTrigger>
+                  <Button>Create</Button>
+                </DropdownTrigger>
+              </NavbarItem>
+              <DropdownMenu>
+                <DropdownItem
+                  key="tournaments"
+                  className="test hover:border-pink-500 hover:border-2"
+                >
+                  <Link href="/create/tournaments">Tournaments</Link>
+                </DropdownItem>
+                <DropdownItem className=" border-pink-700" key="game_category">
+                  <Link
+                    className=" border-pink-700"
+                    href="/create/game-category"
+                  >
+                    Game Category
+                  </Link>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          ) : (
+            <>
+              <NavbarItem>
+                <Link href="/auth/sign-in">Login</Link>
+              </NavbarItem>
+              <NavbarItem>
+                <Button as={Link} color="primary" href="#" variant="flat">
+                  Sign Up
+                </Button>
+              </NavbarItem>
+            </>
+          )}
+        </NavbarContent>
+      </Navbar>
+    </header>
+  );
 }
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,24 +1,29 @@
-import NextAuth, { DefaultSession } from "next-auth"
-import { JWT, DefaultJWT } from "next-auth/jwt"
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+import { JWT, DefaultJWT } from "next-auth/jwt";
 
-declare module 'next-auth' {
-    interface User {
-        username: string
-    }
-    interface Session {
-        user: User & {
-            username: string
-            role: string
-        }
-        token: {
-            username: string
-            role: string
-        } & DefaultSession["user"]
-    }
+declare module "next-auth" {
+  interface User {
+    username: string;
+    role: string;
+  }
+  interface Session {
+    user: User & {
+      username: string;
+      role: string;
+    };
+    token: {
+      username: string;
+      role: string;
+    } & DefaultSession["user"];
+  }
+
+  //   interface User extends DefaultUser {
+  //     role: string;
+  //   }
 }
 
 declare module "next-auth/jwt" {
-    interface JWT extends DefaultJWT {
-        role: string,
-    }
+  interface JWT extends DefaultJWT {
+    role: string | null;
+  }
 }
