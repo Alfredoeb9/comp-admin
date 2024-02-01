981354ddd438867e044a5c15933fd96a9892c9e6
Alfredoeb9 <alfredoeb96@gmail.com>
2024-01-31 17:36:13 -0800

    [rules] updated rules

--- lib/rules.ts
+++ lib/rules.ts
@@ -3,17 +3,19 @@ export const Rules = [
         "mw3": {
             "pc_player": ["Allowed", "Not Allowed"],
             "snaking": ["Allowed", "Not Allowed"],
-            "snipers": ["Allowed", "Not Allowed"]
+            "snipers": ["Allowed", "Not Allowed"],
+            "allowed_input": ["Controller", "MKB", "Controller + MKB"]
         },
 
         "fornight": {
+            "pc_player": ["Allowed", "Not Allowed"],
             "smg":  ["Allowed", "Not Allowed"]
         },
         
-        "testing": {
+        "warzone": {
             "pc_player": ["Allowed", "Not Allowed"],
-            "snaking": ["Allowed", "Not Allowed"],
-            "snipers": ["Allowed", "Not Allowed"]
+            "snipers": ["Allowed", "Not Allowed"],
+            "helicopters": ["Allowed", "Not Allowed"],
         },
     }
 ]
